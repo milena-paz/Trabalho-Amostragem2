874fdb842fb6f525ba4f424b663d2efe37675cd4
milena-paz <milenap4zfreitas@gmail.com>
2026-05-18 19:25:47 -0300

    Update Explora.r

--- Explora.r
+++ Explora.r
@@ -3,3 +3,11 @@ x<-read.csv2("Leitos_2026.csv",encoding="Latin1")%>%
   filter(UF=="MG")
 x$DESC_NATUREZA_JURIDICA[x$DESC_NATUREZA_JURIDICA=="HOSPITAL_FILANTR\xd3PICO"] <- "HOSPITAL_FILANTROPICO"
 x$DESC_NATUREZA_JURIDICA[x$DESC_NATUREZA_JURIDICA=="HOSPITAL_P\xdaBLICO"] <- "HOSPITAL_PUBLICO"
+
+x <- x %>%
+  select(!c("REGIAO","UF","MOTIVO_DESABILITACAO","NO_EMAIL","CO_CEP","NO_COMPLEMENTO"))
+
+# populacao de estudo: leitos dos hospitais do estado de minas gerais vinculados ao SUS referentes ao mes de janeiro
+# amostra: total de leitos dentro dos hospitais do estado de minas gerais vinculados ao SUS (informados pelo DataSUS) referentes ao mês de JANEIRO
+# variável de interesse: número de leitos
+# unidade amostral: leito
