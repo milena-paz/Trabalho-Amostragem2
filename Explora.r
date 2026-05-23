1d90a8299f5be451f75a90f5dc1183db5c1dfed9
milena-paz <milenap4zfreitas@gmail.com>
2026-05-23 13:00:12 -0300

    Update Explora.r

--- Explora.r
+++ Explora.r
@@ -5,9 +5,15 @@ x$DESC_NATUREZA_JURIDICA[x$DESC_NATUREZA_JURIDICA=="HOSPITAL_FILANTR\xd3PICO"] <
 x$DESC_NATUREZA_JURIDICA[x$DESC_NATUREZA_JURIDICA=="HOSPITAL_P\xdaBLICO"] <- "HOSPITAL_PUBLICO"
 
 x <- x %>%
-  select(!c("REGIAO","UF","MOTIVO_DESABILITACAO","NO_EMAIL","CO_CEP","NO_COMPLEMENTO"))
+  select(!c("REGIAO","UF","MOTIVO_DESABILITACAO","NO_EMAIL","CO_CEP","NO_COMPLEMENTO")) %>% 
+  filter(COMP==202601) #referente ao mes de janeiro
 
 # populacao de estudo: leitos dos hospitais do estado de minas gerais vinculados ao SUS referentes ao mes de janeiro
 # amostra: total de leitos dentro dos hospitais do estado de minas gerais vinculados ao SUS (informados pelo DataSUS) referentes ao mês de JANEIRO
 # variável de interesse: número de leitos
-# unidade amostral: leito
+# unidade amostral: leito SUS
+set.seed(65)
+s <- sample(1:nrow(x),size=200,replace=T)
+amostra <- x[s,]
+amostra <- amostra %>%
+  select(c("MUNICIPIO","CNES","NOME_ESTABELECIMENTO","DESC_NATUREZA_JURIDICA","LEITOS_SUS"))
